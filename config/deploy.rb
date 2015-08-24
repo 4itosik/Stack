a4779e04549a62ce41a733b430455f65a49ba9ce
Artem Adamtsov <aay1990@gmail.com>
2015-08-24 14:27:00 +0300

    capistano

---
+++ config/deploy.rb
@@ -0,0 +1,69 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'stack'
+set :repo_url, 'git@github.com:4itosik/Stack.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/stack'
+set :deploy_user, 'deployer'
+
+set :rvm_ruby_version, '2.2.2@stack'
+
+# Default value for :linked_files is []
+set :linked_files, %w(config/database.yml config/private_pub.yml config/private_pub_thin.yml .env)
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
+set :default_shell, '/bin/bash -l'
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
+
+namespace :private_pub do
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml start"
+        end
+      end
+    end
+  end
+
+  desc 'Stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml stop"
+        end
+      end
+    end
+  end
+
+  desc 'Restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
+end
+
+after 'deploy:restart', 'private_pub:restart'
